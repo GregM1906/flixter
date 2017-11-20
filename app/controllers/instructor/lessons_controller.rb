9a4351b208d592b356335c5c70c028a769e55d6b
Greg <gregmarczak@outlook.com>
2017-11-20 19:59:53 +0000

    4.25 video embedding

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -15,7 +15,7 @@ class Instructor::LessonsController < ApplicationController
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
+      return render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
@@ -27,21 +27,4 @@ class Instructor::LessonsController < ApplicationController
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video)
   end
-end
-
-
-#  this line of code
-#  ====>
-#          @current_section ||=
-#  does the same thing as all of these lines of code
-#  ====>
-#          
-#  if @current_section == nil
-#    @current_section = Section.find(params[:section_id])
-#    @current_section
-#  else
-#    @current_section
-#  end
-#  
-#  In short, this single line ( @current_section ||= ) says: if we've looked up the current_section beforehand use the value that we looked up previously. If we haven't looked up this section before, go into the database, look it up and also make sure to remember the value in case we need to look it up again later. This technique of storing certain values inside of the memory to reduce the times we have to find a certain value inside the database is called memoization.
-#
+end 
