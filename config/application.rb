32effc18ab58dc1a90768973c4e349e2dbfe77e1
kenmazaika <ken@thefirehoseproject.com>
2017-11-20 16:09:02 -0800

    Fix problems with fonts and the asset pipeline.

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Flixter
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -18,7 +16,7 @@ module Flixter
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Precompile additional assets
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    config.assets.precompile += %w( .svg .eot .woff .ttf .woff2 )
 
   end
 end
