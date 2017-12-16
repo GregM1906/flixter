56f4324a4522b01951136b85a1eb57b70fd28e78
Greg <gregmarczak@outlook.com>
2017-12-16 02:19:12 +0000

    6.32 sections challenge

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -23,6 +23,12 @@ class Instructor::SectionsController < ApplicationController
 
   private
 
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
   def require_authorized_for_current_course
     if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
